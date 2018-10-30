8f106c7084fdf04b2a368cf5032d3823ab0020c8
S. Gunner <Gunner237@users.noreply.github.com>
2018-10-30 19:43:40 +0000

    Wrapped up in function
    ~ Put it all in a function

--- listadder.py
+++ listadder.py
@@ -1,22 +1,15 @@
-input1str = input('Please enter a list of numbers separated by commas only\n')
-input1strlist = input1str.split(',')
-input1 = list(map(int, input1strlist))
-input2str = input('Please enter a second list of numbers separated by commas only\n')
-input2strlist = input2str.split(',')
-input2 = list(map(int, input2strlist))
-
-input1length = len(input1)
-input2length = len(input2)
-
-shortestlength = min(input1length,input2length)
-
-longerinput = max(input1,input2)
-longerlength = max(input1length,input2length)
-
 def addlist(a, b):
     c = a + b
     return c
+def listadder(input1,input2):
+    input1length = len(input1)
+    input2length = len(input2)
 
-x = list(map(addlist, input1, input2))
-x.extend(longerinput[shortestlength:longerlength])
-print(x)
+    shortestlength = min(input1length,input2length)
+
+    longerinput = max(input1,input2)
+    longerlength = max(input1length,input2length)
+
+    x = list(map(addlist, input1, input2))
+    x.extend(longerinput[shortestlength:longerlength])
+    return x

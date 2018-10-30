df39d44d0034c035c4def324ae352073c0d0d2d0
S. Gunner <Gunner237@users.noreply.github.com>
2018-10-30 17:59:45 +0000

    Update listadder.py
    + Now with actual code!

---
+++ listadder.py
@@ -0,0 +1,19 @@
+input1str = input('Please enter a list of numbers separated by commas only\n')
+input1strlist = input1str.split(',')
+input1 = list(map(int, input1strlist))
+input2str = input('Please enter a second list of numbers separated by commas only (must be shorter than the first)\n')
+input2strlist = input2str.split(',')
+input2 = list(map(int, input2strlist))
+
+input1length = len(input1)
+input2length = len(input2)
+
+shortestlength = min(input1length,input2length)
+
+def addlist(a, b):
+    c = a + b
+        return c
+
+x = list(map(addlist, input1, input2))
+x.extend(input1[shortestlength:input1length])
+print(x)
